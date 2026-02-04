a62669bc0d54e7d19e1472917e69ecf6cd9d4846
Taranjot Singh <65taranjot@gmail.com>
2026-02-04 11:39:00 +0530

    done class 1 work

--- src/SlidingWindow1.java
+++ src/SlidingWindow1.java
@@ -1,5 +1,13 @@
 public class SlidingWindow1 {
+    //array=[2,1,5,1,3,2];
+    //k=3
     public static void main(String[] args) {
-        
+        System.out.println("hi");
+    }
+    public static void main(Integer[] args) {
+        int a=10;
+        int b=20;
+        int c=a+b;
+        System.out.println(c);
     }
 }
